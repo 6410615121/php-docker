d5d18fadf1c3d4b47342014df8c28df52bb017da
suttha jongjaroen <6410615121@cn330>
2024-05-16 22:30:55 +0700

    update index

--- webroot/attack/index.php
+++ webroot/attack/index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index</title>
-</head>
-<body>
-    this is index
-</body>
-</html>
+<?php if(isset($_COOKIE['username'])):  ?>
+    <?php require_once('index_view.php') ?>
+<?php else: ?>
+    <?= "<script>location.href='login.php';</script>" ?>
+<?php endif; ?>
---
+++ webroot/attack/index_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <h2>Hello you are  <a href=""><?=($_COOKIE['username']) ?></a>!!</h2>
+    
+    <h2>comment box</h2>
+    <form action="">
+        <input type="textarea" placeholder="comment something">
+        <input type="submit">
+    </form>
+    
+    <div>
+        
+    </div>
+</body>
+</html>
